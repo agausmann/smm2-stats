4a66242b0240ed9bcefb2df3946abb8ea99f1424
Adam Gausmann <agausmann@fastmail.com>
2022-03-27 16:51:03 -0500

    Add backoff algorithm to bulk downloader to play nice if the API goes down.

--- src/bin/bulk_download_sexpert.rs
+++ src/bin/bulk_download_sexpert.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::{fmt::Display, future::Future, path::Path, time::Duration};
 
 use smm2_stats::mm2_api::{Api, Difficulty};
 
@@ -6,27 +6,30 @@ use smm2_stats::mm2_api::{Api, Difficulty};
 async fn main() -> anyhow::Result<()> {
     let api = Api::default();
     loop {
-        let courses_result = api.search_endless_mode(300, Difficulty::SuperExpert).await;
-        let courses = match courses_result {
-            Ok(x) => x,
-            Err(error) => {
-                eprintln!("search error: {}", error);
-                continue;
-            }
-        };
-
+        let courses = retry_backoff(|| api.search_endless_mode(300, Difficulty::SuperExpert)).await;
         for course in courses {
             println!("{} {}", course.course_id, course.name);
-            let data = loop {
-                let data_result = api.get_level_data(&course.course_id).await;
-                match data_result {
-                    Ok(data) => break data,
-                    Err(error) => {
-                        eprintln!("retry: {}", error);
-                    }
-                }
-            };
+            let data = retry_backoff(|| api.get_level_data(&course.course_id)).await;
             tokio::fs::write(Path::new("levels/sexpert/").join(&course.course_id), &data).await?;
+        }
+    }
+}
+
+async fn retry_backoff<T, E, F, G>(mut generator: F) -> T
+where
+    F: FnMut() -> G,
+    G: Future<Output = Result<T, E>>,
+    E: Display,
+{
+    let mut delay = Duration::from_secs(1);
+    loop {
+        match generator().await {
+            Ok(x) => return x,
+            Err(error) => {
+                eprintln!("retry: {}", error);
+                tokio::time::sleep(delay).await;
+                delay = (delay * 2).max(Duration::from_secs(60));
+            }
         }
     }
 }
