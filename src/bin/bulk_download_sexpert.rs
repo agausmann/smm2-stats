3ab1620272aa92c8fc21b86703a32b2abfc47c30
Adam Gausmann <agausmann@fastmail.com>
2022-03-27 11:19:44 -0500

    bulk_download_sexpert: Update to run indefinitely and fix output path

--- src/bin/bulk_download_sexpert.rs
+++ src/bin/bulk_download_sexpert.rs
@@ -5,21 +5,28 @@ use smm2_stats::mm2_api::{Api, Difficulty};
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let api = Api::default();
-    let courses = api
-        .search_endless_mode(200, Difficulty::SuperExpert)
-        .await?;
-    for course in courses {
-        println!("{} {}", course.course_id, course.name);
-        let data = loop {
-            let data_result = api.get_level_data(&course.course_id).await;
-            match data_result {
-                Ok(data) => break data,
-                Err(error) => {
-                    eprintln!("retry: {}", error);
-                }
+    loop {
+        let courses_result = api.search_endless_mode(300, Difficulty::SuperExpert).await;
+        let courses = match courses_result {
+            Ok(x) => x,
+            Err(error) => {
+                eprintln!("search error: {}", error);
+                continue;
             }
         };
-        tokio::fs::write(Path::new("sexpert/").join(&course.course_id), &data).await?;
+
+        for course in courses {
+            println!("{} {}", course.course_id, course.name);
+            let data = loop {
+                let data_result = api.get_level_data(&course.course_id).await;
+                match data_result {
+                    Ok(data) => break data,
+                    Err(error) => {
+                        eprintln!("retry: {}", error);
+                    }
+                }
+            };
+            tokio::fs::write(Path::new("levels/sexpert/").join(&course.course_id), &data).await?;
+        }
     }
-    Ok(())
 }
