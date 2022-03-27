2bc8f36685f218db8a91523dd9c525a99d3c6924
Adam Gausmann <agausmann@fastmail.com>
2022-03-26 23:49:41 -0500

    Add retry mechanism to bulk downloader

--- src/bin/bulk_download_sexpert.rs
+++ src/bin/bulk_download_sexpert.rs
@@ -10,7 +10,15 @@ async fn main() -> anyhow::Result<()> {
         .await?;
     for course in courses {
         println!("{} {}", course.course_id, course.name);
-        let data = api.get_level_data(&course.course_id).await?;
+        let data = loop {
+            let data_result = api.get_level_data(&course.course_id).await;
+            match data_result {
+                Ok(data) => break data,
+                Err(error) => {
+                    eprintln!("retry: {}", error);
+                }
+            }
+        };
         tokio::fs::write(Path::new("sexpert/").join(&course.course_id), &data).await?;
     }
     Ok(())
